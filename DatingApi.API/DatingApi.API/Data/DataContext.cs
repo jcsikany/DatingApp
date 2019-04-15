4e9319f6706e9a91b826bf5500ac9dbde3d01e0f
juan pablo <jcsikany@gmail.com>
2019-04-15 19:26:33 -0300

    Initial Commit

---
+++ DatingApi.API/DatingApi.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using DatingApi.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApi.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        // Values gonna be the table name, is conventional to pluralize the name of the entities
+        public DbSet<Value> Values { get; set; }
+    }
+}
